59cc33e255c451bff0917f8eeda812c8426ce6cd
afoucart <afoucart@emeal.nttdata.com>
2022-09-23 15:14:37 +0200

    [WIP] Code documentation
    Documented CoreNavigationProvider

--- core-navigation/src/main/java/com/rocket/android/core/navigation/di/CoreNavigationProvider.kt
+++ core-navigation/src/main/java/com/rocket/android/core/navigation/di/CoreNavigationProvider.kt
@@ -2,6 +2,13 @@ package com.rocket.android.core.navigation.di
 
 import com.rocket.android.core.navigation.NavigatorLifecycle
 
+/**
+ * Class which contains an instance of [NavigatorLifecycle]
+ *
+ * Provides a singleton instance of [NavigatorLifecycle], which manages app's lifecycle
+ *
+ * @property provideNavigatorLifecycle instance of [NavigatorLifecycle]
+ */
 @Suppress("unused")
 class CoreNavigationProvider private constructor() {
     val provideNavigatorLifecycle: NavigatorLifecycle by lazy {
