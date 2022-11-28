2da0cb9e1146f0fdddc26331fda57815e3c44bd4
afoucart <afoucart@emeal.nttdata.com>
2022-11-28 09:45:53 +0100

    [WIP] Code documentation
    Documented some methods in BaseNavigator

--- core-navigation/src/main/java/com/rocket/android/core/navigation/BaseNavigator.kt
+++ core-navigation/src/main/java/com/rocket/android/core/navigation/BaseNavigator.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import androidx.annotation.IdRes
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.PopUpToBuilder
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -32,9 +34,25 @@ import kotlinx.coroutines.launch
  * @property fragment currently active primary navigation fragment for the [activity] *FragmentManager*
  */
 sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
+
+    /**
+     * App navigator
+     *
+     * Navigator intended to be used as the one available in all sections of the app. It may be used to navigate to
+     * screens available from multiple places
+     *
+     * @property navigatorLifecycle instance of [NavigatorLifecycle]
+     */
     abstract class AppBaseNavigator(navigatorLifecycle: NavigatorLifecycle) :
         BaseNavigator(navigatorLifecycle = navigatorLifecycle)
 
+    /**
+     * Feature navigator
+     *
+     * Navigator intended to be used as the one for an specific app flow
+     *
+     * @property navigatorLifecycle instance of [NavigatorLifecycle]
+     */
     abstract class FeatureBaseNavigator(navigatorLifecycle: NavigatorLifecycle) :
         BaseNavigator(navigatorLifecycle = navigatorLifecycle)
 
@@ -50,6 +68,13 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
     internal val fragment: Fragment?
         get() = activity?.supportFragmentManager?.primaryNavigationFragment
 
+    /**
+     * If [reload] is false, it will try to find a fragment which extends from [Type] inside fragment's child fragment
+     * manager
+     * @param Type type of the fragment to be found
+     * @param reload determines whether to search or not the fragment
+     * @return false if a fragment has been found, otherwise returns true. Also returns true if [reload] is true
+     */
     protected inline fun <reified Type : Fragment> canReload(reload: Boolean): Boolean {
         var canReload = !reload
         if (canReload) {
@@ -62,6 +87,14 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
         return canReload
     }
 
+    /**
+     * If [clear] is true, constructs a new [NavOptions] using [to] and a new [PopUpToBuilder] built with
+     * [popUpToInclusive] as parameters for the [NavOptionsBuilder.popUpTo] method
+     * @param clear determines whether to create or not the builder
+     * @param to indicates the destination to pop up to
+     * @param popUpToInclusive whether the *to* destination should be popped from the back stack
+     * @return if clear is true, the [NavOptions] built, otherwise returns null
+     */
     protected fun clearBackStackTo(
         clear: Boolean,
         @IdRes to: Int,
@@ -80,18 +113,31 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
     }
 
     // region FRAGMENT
+    /**
+     * Calls [popBackStack] method on [fragment] inside the Main thread
+     */
     fun goBack() {
         checkMainThread {
             fragment?.popBackStack()
         }
     }
 
+    /**
+     * Calls [popBackStackFragment] (with [Type] as parameter) on [fragment] inside the Main thread
+     * @param [Type]
+     */
     inline fun <reified Type : Fragment> goBackFragment() {
         checkMainThread {
             fragment?.popBackStackFragment<Type>()
         }
     }
 
+    /**
+     * Calls [navigate] (with [id], [navOptions] and [extras] as its parameters) on [fragment] inside the Main thread
+     * @param id *id* parameter of the [navigate] method
+     * @param navOptions *navOptions* parameter of the [navigate] method
+     * @param extras *extras* parameter of the [navigate] method
+     */
     fun goTo(
         @IdRes id: Int,
         navOptions: NavOptions? = null,
@@ -102,6 +148,14 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
         }
     }
 
+    /**
+     * Calls [navigateFragment] (with [Type], [id], [navOptions] and [extras] as its parameters) on [fragment] inside
+     * the Main thread
+     * @param Type the type of the FragmentManager.getPrimaryNavigationFragment to be found
+     * @param id *id* parameter of the [navigateFragment] method
+     * @param navOptions *navOptions* parameter of the [navigateFragment] method
+     * @param extras *extras* parameter of the [navigateFragment] method
+     */
     inline fun <reified Type : Fragment> goToFragment(
         @IdRes id: Int,
         navOptions: NavOptions? = null,
@@ -112,6 +166,14 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
         }
     }
 
+    /**
+     * Calls the two-argument overload (using [NavDirections]) of [navigate] (with [directions] and [navOptions] as its
+     * arguments) on [fragment] inside the Main thread
+     * @param directions *directions* parameter of the two-argument overload (using [NavDirections]) of the [navigate]
+     * method
+     * @param navOptions *navOptions* parameter of the two-argument overload (using [NavDirections]) of the [navigate]
+     * method
+     */
     fun goTo(
         directions: NavDirections,
         navOptions: NavOptions? = null
@@ -121,6 +183,16 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
         }
     }
 
+    /**
+     * Calls the three-argument overload (using [NavDirections]) of [navigateFragment] (with [Type], [directions] and
+     * [navOptions] as its arguments) on [fragment] inside the Main thread
+     * @param Type *Type* parameter of the three-argument overload (using [NavDirections]) of the [navigateFragment]
+     * method
+     * @param directions *directions* parameter of the three-argument overload (using [NavDirections]) of the
+     * [navigateFragment] method
+     * @param navOptions *navOptions* parameter of the three-argument overload (using [NavDirections]) of the
+     * [navigateFragment] method
+     */
     inline fun <reified Type : Fragment> goToFragment(
         directions: NavDirections,
         navOptions: NavOptions? = null
@@ -130,6 +202,12 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
         }
     }
 
+    /**
+     * Calls the two-argument overload (using [Uri]) of [navigate] (with [uri] and [navOptions] as its arguments) on
+     * [fragment] inside the Main thread
+     * @param uri *uri* parameter of the two-argument overload (using [Uri]) of the [navigate] method
+     * @param navOptions *navOptions* parameter of the two-argument overload (using [Uri]) of the [navigate] method
+     */
     fun goTo(
         uri: Uri,
         navOptions: NavOptions? = null
@@ -175,6 +253,10 @@ sealed class BaseNavigator(private val navigatorLifecycle: NavigatorLifecycle) {
     // endregion
 
     // region UTILS
+    /**
+     * Executes [block] on the Main thread
+     * @param block Lambda to be executed
+     */
     @PublishedApi
     internal fun checkMainThread(block: () -> Unit) {
         if (Thread.currentThread().isDaemon) {
