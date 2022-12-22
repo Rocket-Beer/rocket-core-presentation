f07dc12d94c555fe885e86ceba6509fecd5155fe
afoucart <afoucart@emeal.nttdata.com>
2022-12-22 17:21:19 +0100

    [WIP] Core-data-map documentation
    Commented Polygon file

--- core-data-map/src/main/java/com/rocket/android/core/data/map/model/Polygon.kt
+++ core-data-map/src/main/java/com/rocket/android/core/data/map/model/Polygon.kt
@@ -3,16 +3,34 @@ package com.rocket.android.core.data.map.model
 import com.google.android.gms.maps.model.Polygon as GmsPolygon
 import com.huawei.hms.maps.model.Polygon as HmsPolygon
 
+/**
+ * TODO
+ *
+ * TODO
+ *
+ * @property hmsPolygon Instance of [HmsPolygon]
+ */
 class Polygon {
     var gmsPolygon: GmsPolygon? = null
     var hmsPolygon: HmsPolygon? = null
 
+    /**
+     * Removes [hmsPolygon] from the map
+     */
     fun remove() {
         gmsPolygon?.remove()
         hmsPolygon?.remove()
     }
 }
 
+/**
+ * Creates a [Polygon] from a [GmsPolygon]
+ * @return a [Polygon] object with its [Polygon.gmsPolygon] as this polygon
+ */
 fun GmsPolygon.toPolygon() = Polygon().also { it.gmsPolygon = this }
 
+/**
+ * Creates a [Polygon] from a [HmsPolygon]
+ * @return a [Polygon] object with its [Polygon.hmsPolygon] as this polygon
+ */
 fun HmsPolygon.toPolygon() = Polygon().also { it.hmsPolygon = this }
