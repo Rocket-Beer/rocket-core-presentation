f4c558a5852a1504a65dbb29d4496c128c966f41
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-03-07 08:45:29 +0100

    [WIP] - create module and convert gradle to kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "Android Architecture Core Presentation"
-include(":core-viewmodel", ":core-navigation", ":core-data-map", ":core-data-map-gms", ":sample-app")
+include(
+    ":core-viewmodel",
+    ":core-navigation",
+    ":core-data-map",
+    ":core-data-map-gms",
+    ":sample-app",
+    ":core-navigation-compose"
+)
