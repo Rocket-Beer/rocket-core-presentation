9a27c4ab8b45ec297c8196737432f71a20f9a5f9
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-03-07 10:24:47 +0100

    [WIP] - use annotation for remove warnings

--- core-navigation-compose/src/main/java/com/rocket/android/core/navigation/compose/di/CoreNavigatorComposeProvider.kt
+++ core-navigation-compose/src/main/java/com/rocket/android/core/navigation/compose/di/CoreNavigatorComposeProvider.kt
@@ -2,6 +2,7 @@ package com.rocket.android.core.navigation.compose.di
 
 import com.rocket.android.core.navigation.compose.NavigatorManager
 
+@Suppress("unused")
 class CoreNavigatorComposeProvider private constructor() {
 
     val provideNavigatorManager: NavigatorManager by lazy {
