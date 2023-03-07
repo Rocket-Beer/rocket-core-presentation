76407d2dc2b9208655620c2841ed342d66673bb1
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2023-03-07 10:14:32 +0100

    [WIP] - create provider

---
+++ core-navigation-compose/src/main/java/com/rocket/android/core/navigation/compose/di/CoreNavigatorComposeProvider.kt
@@ -0,0 +1,16 @@
+package com.rocket.android.core.navigation.compose.di
+
+import com.rocket.android.core.navigation.compose.NavigatorManager
+
+class CoreNavigatorComposeProvider private constructor() {
+
+    val provideNavigatorManager: NavigatorManager by lazy {
+        NavigatorManager()
+    }
+
+    companion object {
+        val instance: CoreNavigatorComposeProvider by lazy {
+            CoreNavigatorComposeProvider()
+        }
+    }
+}
