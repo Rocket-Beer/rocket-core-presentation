378784fd1dace66443ce9332b1b95617f4a0a3d1
jesusdesantos <jdesanto@emeal.nttdata.com>
2023-12-04 16:51:55 +0100

    [WIP] - Update isDownloadedFromStore function

--- core-security-utils-presentation/src/main/java/com/rocket/android/core/security/SecurityUtils.kt
+++ core-security-utils-presentation/src/main/java/com/rocket/android/core/security/SecurityUtils.kt
@@ -24,11 +24,7 @@ fun isRoot(): Boolean {
  * This function is used to check whether an application is running on an emulator.
  */
 fun isEmulator(): Boolean {
-    return (Build.FINGERPRINT.startsWith("generic") || Build.FINGERPRINT.startsWith("unknown") || Build.MODEL.contains(
-        "google_sdk"
-    ) || Build.MODEL.contains("Emulator") || Build.MODEL.contains("Android SDK built for x86") || Build.MANUFACTURER.contains(
-        "Genymotion"
-    ) || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic") || "google_sdk" == Build.PRODUCT)
+    return (Build.FINGERPRINT.startsWith("generic") || Build.FINGERPRINT.startsWith("unknown") || Build.MODEL.contains("google_sdk") || Build.MODEL.contains("Emulator") || Build.MODEL.contains("Android SDK built for x86") || Build.MANUFACTURER.contains("Genymotion") || Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic") || "google_sdk" == Build.PRODUCT)
 }
 
 /**
